19ef069ab45bac4129c563a50ff5977d6893b70c
Maxim-LD <ayinde779@gmail.com>
2024-07-24 21:37:03 +0100

    script added

---
+++ script.js
@@ -0,0 +1,34 @@
+// String variables
+let greeting = "Hello, world!";
+let myName = "Arowosere Ayodeji";
+let state = "Niger State"; 
+let hobby = "Writing codes";
+let sport = "Football";
+
+// number variables
+let age = 25;
+let year = 2024;
+let negativeNumber = -7;
+let accountNo = 90678456378;
+let decimalNumber = 0.001;
+
+// Boolean variables
+let isActive = true;
+let isMarried = false;
+let firstBorn = false;
+let onlyMale = true;
+let isPositive = true;
+
+// Null variables
+let accountInfo = null;
+let officeAddress = null;
+let level = null;
+let status = null;
+let login = null;
+
+// undefined variables
+let myAge = undefined;
+let fuelCost = undefined;
+let schoolFees = undefined;
+let systemModel = undefined;
+let weather = undefined;
